801c4a0cc9d62ec9e9fee05a813e9ca083d47a85
faith <faithmueni6@gmail.com>
2023-05-15 14:50:41 +0300

    added folders

---
+++ Day1/dayOne.py
@@ -0,0 +1,8 @@
+
+
+# ### Day 1: Python Basics
+# Start by installing Python and a text editor. Learn the basics of Python, such as: 
+#  variables, data types, Math Operators, Comments, Input() function, Print() function, The str(), int(), and float() Functions
+
+# #### Exercise
+# Simple Python Calculator Program: Write a simple program that does basic math operations including addition, subtraction, deletion, division, power, squareroot,etc,. 
