1b689468aa40919c317a12866780d114c1d3da8c
faith <faithmueni6@gmail.com>
2023-05-15 22:42:44 +0300

    Done with Day 1

--- Day1/dayOne.py
+++ Day1/dayOne.py
@@ -6,3 +6,28 @@
 
 # #### Exercise
 # Simple Python Calculator Program: Write a simple program that does basic math operations including addition, subtraction, deletion, division, power, squareroot,etc,. 
+
+
+def PythonCalculator():
+    numOne= int(input("Please Enter number 1: "))
+    numTwo= int(input("Please Enter number 2: "))
+
+    operand = int(input("Enter the Operand '\n' 1:add '\n' 2:subtract '\n' 3:multiply,'\n' 4:divide: '\n' "))
+
+    if operand ==1:
+        return numOne + numTwo
+    elif operand ==2:
+        return numOne - numTwo
+    elif operand ==3:
+        return numOne * numTwo
+    elif operand == 4:
+        return numOne + numTwo
+    else:
+        return "You entered the wrong operand"
+
+if __name__ == "__main__":
+    result = PythonCalculator()
+    print(result)
+
+
+
